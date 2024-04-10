cdd642c0aff1565f29c996e36bb943d1c1f92271
Вика Александрова <viktoriya_job@mail.ru>
2024-04-10 20:35:57 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Testiny
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
